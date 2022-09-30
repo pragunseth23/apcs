b46ac34de4d05549b95f9ce8e47092ffa7edf715
Pragun Seth <pragunseth23@gmail.com>
2022-09-29 22:41:41 -0400

    several functions two, three

--- severalfunctions/three.java
+++ severalfunctions/three.java
@@ -1,7 +1,49 @@
 package severalfunctions;
 
-public class three {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class three {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int cap = 20;
+        Shape arr[] = new Shape[rand.nextInt(cap)];
+
+        for (int i = 0; i < arr.length; i++) {
+            Shape ok = new Shape();
+            arr[i] = ok;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getColor() == "red") {
+                arr[i].toBlack();
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+class Shape {
+    Random rand = new Random();
+    String[] typeArr = { "Cube", "Triangle", "Cone" };
+    String[] colorArr = { "red", "green", "blue", "yellow", "orange" };
+    int typeIndex = rand.nextInt(typeArr.length);
+    private String type = typeArr[typeIndex];
+    int colorIndex = rand.nextInt(colorArr.length);
+    private String color = colorArr[colorIndex];
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void toBlack() {
+        color = "black";
+    }
+
+    public String toString() {
+        String answer = "[" + this.type + ", " + this.color + "]";
+        return answer;
     }
 }
--- severalfunctions/two.java
+++ severalfunctions/two.java
@@ -1,7 +1,41 @@
 package severalfunctions;
 
+import java.util.*;
+
 public class two {
+
     public static void main(String[] args) {
+        Random rand = new Random();
+        int amt = 20;
+        int arr[] = new int[rand.nextInt(amt)];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(amt);
+        }
+        System.out.println(Arrays.toString(arr));
+        changeNum(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void changeNum(int input[]) {
+        Random rand = new Random();
+        List<Integer> givenList = Arrays.asList(1, 2, 3);
+        int randomElement = givenList.get(rand.nextInt(givenList.size()));
+        System.out.println(randomElement);
+        if (randomElement == 1) {
+            for (int i = 0; i < input.length; i++) {
+                input[i] = 999;
+            }
+        }
+        if (randomElement == 2) {
+            for (int i = 0; i < input.length; i = i + 2) {
+                input[i] = 555;
+            }
+        }
+        if (randomElement == 3) {
+            for (int i = 0; i < input.length; i = i + 3) {
+                input[i] = 444;
+            }
+        }
 
     }
 }
