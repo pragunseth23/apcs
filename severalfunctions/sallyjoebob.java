8a206639e6d5b721a305089b5bf5aada8a133da9
Pragun Seth <pragunseth23@gmail.com>
2022-10-03 12:34:43 -0400

    sallyjoebob

---
+++ severalfunctions/sallyjoebob.java
@@ -0,0 +1,14 @@
+package severalfunctions;
+
+public class sallyjoebob {
+    public static void main(String[] args) {
+        String x = "sallyjoebob";
+        String y = new String("sallyjoebob");
+        if (x == "sallyjoebob") {
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("strings are unequal");
+        }
+    }
+
+}
